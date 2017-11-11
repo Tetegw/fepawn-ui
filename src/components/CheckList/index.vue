cc3156157c421126e9a8b0c03b4391078193ec89
Tetegw <1151832942@qq.com>
2017-11-11 21:59:36 +0800

    [Added] checkList style init

--- src/components/CheckList/index.vue
+++ src/components/CheckList/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="fepawn-check-list">
-    <div><input type="checkbox" v-model="checkValue" value="北京">北京</div>
-    <div><input type="checkbox" v-model="checkValue" value="上海">上海</div>
-    <div><input type="checkbox" v-model="checkValue" value="深圳">深圳</div>
-    <div><input type="checkbox" v-model="checkValue" value="苏州">苏州</div>
+    <template v-for="(item, index) in checkList">
+      <div :key="index" class="check-list-item border-top-1px">
+        <div class="checkBtn checked">
+          <input type="checkbox" v-model="checkValue" :value="item.value">
+        </div>
+        <div class="checkInfo">{{item.name}}</div>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -11,6 +15,24 @@
 export default {
   data () {
     return {
+      checkList: [
+        {
+          name: '北京',
+          value: '北京'
+        },
+        {
+          name: '上海',
+          value: '上海'
+        },
+        {
+          name: '深圳',
+          value: '深圳'
+        },
+        {
+          name: '苏州',
+          value: '苏州'
+        }
+      ],
       checkValue: []
     }
   },
@@ -23,4 +45,42 @@ export default {
 </script>
 
 <style>
+@import url('../../assets/style/index.css');
+.check-list-item{
+  text-align: left;
+  line-height: 30px;
+  margin-left: 20px;
+  padding: 10px 15px 10px 0;
+}
+.checkBtn{
+  display: inline-block;
+  vertical-align: middle;
+  width: 40px;
+  height: 32px;
+  position: relative;
+}
+.checkBtn::before{
+  content: '';
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+  width: 20px;
+  height: 20px;
+  border: 1px solid #999;
+  border-radius: 50%;
+}
+.checkBtn.checked::before{
+  border: 1px solid red;
+}
+.checkBtn input{
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+}
+.checkInfo {
+  display: inline-block;
+  vertical-align: middle;  
+  line-height: 32px;
+}
 </style>
--- src/components/ModulePage/checkListPage.vue
+++ src/components/ModulePage/checkListPage.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="fullPage">
+  <div class="fullPage bgfbf9fe">
+    <p>一般使用</p>
+    <div class="border-top-1px border-bot-1px">
       <check-list></check-list>
+    </div>
+    
   </div>
 </template>
 
@@ -12,3 +16,15 @@ export default {
   }
 }
 </script>
+
+<style>
+.fullPage.bgfbf9fe{
+  background-color:#fbf9fe;
+}
+.fullPage.bgfbf9fe p{
+  text-align: left;
+  padding-left: 10px;
+  line-height: 40px;
+}
+</style>
+
