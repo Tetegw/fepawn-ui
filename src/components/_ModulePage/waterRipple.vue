304874e944bbd233988eabb8cdb6cad57958b596
孙晨 <1151832942@qq.com>
2017-12-12 14:30:21 +0800

    [Add] bubble component add

--- src/components/_ModulePage/waterRipple.vue
+++ src/components/_ModulePage/waterRipple.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="full-page">
     <div class="water-ripple-wrap">
-      <water-ripple
-        :value="value"
-        :wrapSize="wrapSize"
-      ></water-ripple>
+      <water-ripple :value="value" :wrapSize="wrapSize"></water-ripple>
     </div>
     <input class="range" type="range" v-model="rangeValue" min="0" max="100" @change="rangeChange">
     <div>当前比例：{{value}}</div>
+    <bubble :y="10"></bubble>
   </div>
 </template>
 
 <script>
-import waterRipple from '@/components/WaterRipple'
+import WaterRipple from '@/components/WaterRipple'
+import Bubble from '@/components/WaterRipple/bubble'
 export default {
   data () {
     return {
@@ -27,17 +26,18 @@ export default {
     }
   },
   components: {
-    'water-ripple': waterRipple
+    'water-ripple': WaterRipple,
+    bubble: Bubble
   }
 }
 </script>
 
 <style>
 @import url('../../assets/style/index.css');
-.range{
+.range {
   height: 30px;
 }
-.water-ripple-wrap{
+.water-ripple-wrap {
   height: 200px;
   padding: 20px;
 }
