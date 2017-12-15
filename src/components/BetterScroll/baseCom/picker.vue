de47aa14a6d54e57cb5cd9c34816e5db25d1686e
孙晨 <1151832942@qq.com>
2017-12-15 17:37:20 +0800

    [Added]  picker component done

--- src/components/BetterScroll/baseCom/picker.vue
+++ src/components/BetterScroll/baseCom/picker.vue
@@ -3,9 +3,9 @@
 		<div class="picker-panel" v-show="state === 1">
 			<!-- header -->
 			<div class="picker-choose border-bot-1px">
-				<span class="cancel">cancelTxt</span>
-				<span class="confirm">confirmTxt</span>
-				<h1 class="picker-title">title</h1>
+				<span class="cancel" @click="cancel">{{cancelTxt}}</span>
+				<span class="confirm" @click.stop="confirm">{{confirmTxt}}</span>
+				<h1 class="picker-title">{{title}}</h1>
 			</div>
 			<!-- content -->
 			<div class="picker-content">
@@ -30,6 +30,10 @@
 <script>
 import BScroll from 'better-scroll'
 const COMPONENT_NAME = 'picker'
+
+const EVENT_CHANGE = 'change'     // 取消，滚动结束触发事件
+const EVENT_SELECT = 'select'     // 点击确定，选中触发事件
+const EVENT_VALUE_CHANGE = 'valueChange'    // 点击确定，当value值发生变化时触发
 const STATE_HIDE = 0
 const STATE_SHOW = 1
 
@@ -39,7 +43,9 @@ export default {
     return {
       state: STATE_HIDE,
       pickerData: this.data.slice(),
-      pickerSelectedIndex: this.selectedIndex // picker选中索引
+      pickerSelectedIndex: this.selectedIndex, // picker选中索引，数组
+      pickerSelectedVal: [],
+      pickerSelectedText: []
     }
   },
   props: {
@@ -54,11 +60,11 @@ export default {
     },
     cancelTxt: {
       type: String,
-      default: 'cancel'
+      default: '取消'
     },
     confirmTxt: {
       type: String,
-      default: 'confirm'
+      default: '确认'
     },
     selectedIndex: {
       type: Array,
@@ -78,6 +84,11 @@ export default {
       this.pickerData.forEach((el, index) => {
         this.pickerSelectedIndex[index] = 0
       })
+    }
+  },
+  watch: {
+    data (newData) {
+      this.setData(newData)
     }
   },
   methods: {
@@ -103,9 +114,43 @@ export default {
         })
       }
     },
-    cancel () {
-      // 隐藏
+    hide () {
       this.state = STATE_HIDE
+      this.pickerData.forEach((element, i) => {
+        this.wheels[i].disable()
+      })
+    },
+    cancel () {
+      this.hide()
+      // 发射事件，不带参数
+      this.$emit(EVENT_CHANGE)
+    },
+    confirm () {
+      if (!this._canConfirm()) {
+        return
+      }
+      this.hide()
+      let changed = false
+      this.pickerData.forEach((element, i) => {
+        // 获取到每一个wheel选中的索引
+        let index = this.wheels[i].getSelectedIndex()
+        this.pickerSelectedIndex[i] = index
+        // 根据索引获取value以及text
+        let newValue = null
+        if (element.length) {
+          newValue = element[index].value
+        }
+        if (this.pickerSelectedVal[i] !== newValue) {
+          changed = true
+        }
+        this.pickerSelectedVal[i] = element[index].value
+        this.pickerSelectedText[i] = element[index].text
+      })
+
+      this.$emit(EVENT_SELECT, this.pickerSelectedVal, this.pickerSelectedIndex, this.pickerSelectedText)
+      if (changed) {
+        this.$emit(EVENT_VALUE_CHANGE, this.pickerSelectedVal, this.pickerSelectedIndex, this.pickerSelectedText)
+      }
     },
     _createWheel (wheelWrapper, i) {
       if (!this.wheels[i]) {
@@ -119,21 +164,27 @@ export default {
           probeType: 3
         })
         this.wheels[i].on('scrollEnd', () => {
-          console.log(i, this.wheels[i].getSelectedIndex())
-          this.$emit('change', i, this.wheels[i].getSelectedIndex())
+          this.$emit(EVENT_CHANGE, i, this.wheels[i].getSelectedIndex())
         })
       } else {
         this.wheels[i].refresh()
       }
       return this.wheels[i]
+    },
+    _canConfirm () {
+      // 每一个wheel都不再动画了，即可以确定提交
+      return this.wheels.every((wheel) => {
+        console.log(!wheel.isInTransition)
+        return !wheel.isInTransition
+      })
     }
   }
 }
 </script>
 
 <style scoped>
-@import url('../../../assets/style/index.css');
-.picker{
+@import url("../../../assets/style/index.css");
+.picker {
   position: fixed;
   left: 0;
   top: 0;
@@ -143,15 +194,17 @@ export default {
   overflow: hidden;
   text-align: center;
   font-size: 14px;
-  background-color: rgba(37, 38, 45, .4);
+  background-color: rgba(37, 38, 45, 0.4);
 }
-.picker .picker-fade-enter, .picker .picker-fade-leave-to{
-  opacity: 0
+.picker .picker-fade-enter,
+.picker .picker-fade-leave-to {
+  opacity: 0;
 }
-.picker .picker-fade-enter-active, .picker .picker-fade-leave-active{
-  transition: all .3s ease-in-out
+.picker .picker-fade-enter-active,
+.picker .picker-fade-leave-active {
+  transition: all 0.3s ease-in-out;
 }
-.picker-panel{
+.picker-panel {
   position: absolute;
   z-index: 600;
   bottom: 0;
@@ -160,20 +213,22 @@ export default {
   background: #fff;
 }
 
-.picker-panel .picker-move-enter, .picker-panel .picker-move-leave-to{
-  transform: translate3d(0, 273px, 0)
+.picker-panel .picker-move-enter,
+.picker-panel .picker-move-leave-to {
+  transform: translate3d(0, 273px, 0);
 }
-.picker-panel .picker-move-enter-active, .picker-panel .picker-move-leave-active{
-  transition: all .3s ease-in-out
+.picker-panel .picker-move-enter-active,
+.picker-panel .picker-move-leave-active {
+  transition: all 0.3s ease-in-out;
 }
 
-.picker-choose{
+.picker-choose {
   position: relative;
   height: 60px;
   color: #999;
 }
-        
-.picker-choose .picker-title{
+
+.picker-choose .picker-title {
   margin: 0;
   line-height: 60px;
   font-weight: normal;
@@ -181,52 +236,62 @@ export default {
   font-size: 18px;
   color: #333;
 }
-  
-.picker-choose .confirm, .picker-choose .cancel{
+
+.picker-choose .confirm,
+.picker-choose .cancel {
   position: absolute;
   top: 6px;
   padding: 16px;
   font-size: 14px;
 }
-.picker-choose .confirm{
+.picker-choose .confirm {
   right: 0;
   color: #007bff;
 }
-.picker-choose .confirm:active{
+.picker-choose .confirm:active {
   color: #5aaaff;
 }
-.picker-choose .cancel{
+.picker-choose .cancel {
   left: 0;
 }
-.picker-choose .cancel:active{
+.picker-choose .cancel:active {
   color: #fdc2a5;
 }
-.picker-choose .picker-content{
+.picker-content {
   position: relative;
   top: 20px;
 }
-.mask-top, .mask-bottom{
+.mask-top,
+.mask-bottom {
   z-index: 10;
   width: 100%;
   height: 68px;
   pointer-events: none;
   transform: translateZ(0);
 }
-.mask-top{
+.mask-top {
   position: absolute;
   top: 0;
-  background: linear-gradient(to top, rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.8));
+  background: linear-gradient(
+    to top,
+    rgba(255, 255, 255, 0.4),
+    rgba(255, 255, 255, 0.8)
+  );
 }
-.mask-bottom{
+.mask-bottom {
   position: absolute;
   bottom: 1px;
-  background: linear-gradient(to bottom, rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.8));
+  background: linear-gradient(
+    to bottom,
+    rgba(255, 255, 255, 0.4),
+    rgba(255, 255, 255, 0.8)
+  );
 }
-.wheel-wrapper{
+.wheel-wrapper {
   display: flex;
   padding: 0 16px;
 }
-.wheel{
+.wheel {
   -ms-flex: 1 1 0.000000001px;
   -webkit-box-flex: 1;
   -webkit-flex: 1;
@@ -238,20 +303,20 @@ export default {
   overflow: hidden;
   font-size: 20px;
 }
-.wheel-scroll{
+.wheel-scroll {
   padding: 0;
   margin-top: 68px;
   line-height: 36px;
   list-style: none;
 }
-.wheel-item{
+.wheel-item {
   list-style: none;
   height: 36px;
   overflow: hidden;
   white-space: nowrap;
   color: #333;
 }
-.picker-footer{
-  height: 20px
+.picker-footer {
+  height: 20px;
 }
 </style>
--- src/components/BetterScroll/pickerPage.vue
+++ src/components/BetterScroll/pickerPage.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="bscroll-picker">
-    <div class="one-column column" @click="showPicker(0)">one-column</div>
+    <div class="one-column column" @click="showPicker(0)">{{oneColumn}}</div>
     <picker
-    ref="picker0"
-    :data="oneData"
+      ref="picker0"
+      :data="oneData"
+      @valueChange="valueChange(0, arguments)"
     ></picker>
-    <div class="two-column column">two-column</div>
-    <div class="three-column column">three-column</div>
+    <div class="two-column column" @click="showPicker(1)">{{twoColumn}}</div>
+    <picker
+      ref="picker1"
+      :data="twoData"
+      @valueChange="valueChange(1, arguments)"
+    ></picker>
+    <div class="three-column column" @click="showPicker(2)">{{threeColumn}}</div>
+    <picker
+      ref="picker2"
+      :data="threeData"
+      @valueChange="valueChange(2, arguments)"
+    ></picker>
     <div class="linkage-column column">linkage-column</div>
   </div>
 </template>
@@ -16,7 +27,262 @@ import Picker from '@/components/BetterScroll/baseCom/picker'
 export default {
   data () {
     return {
+      oneColumn: 'oneColumn',
+      twoColumn: 'twoColumn',
+      threeColumn: 'threeColumn',
       oneData: [
+        [
+          {
+            text: '剧毒',
+            value: 1
+          },
+          {
+            text: '蚂蚁',
+            value: 2
+          },
+          {
+            text: '幽鬼',
+            value: 3
+          },
+          {
+            text: '主宰',
+            value: 4
+          },
+          {
+            text: '卡尔',
+            value: 5
+          },
+          {
+            text: '宙斯',
+            value: 6
+          },
+          {
+            text: '巫医',
+            value: 7
+          },
+          {
+            text: '巫妖',
+            value: 8
+          },
+          {
+            text: '神谕者',
+            value: 9
+          },
+          {
+            text: '撼地神牛',
+            value: 10
+          },
+          {
+            text: '蓝胖子',
+            value: 11
+          },
+          {
+            text: '水晶室女',
+            value: 12
+          },
+          {
+            text: '莉娜',
+            value: 13
+          },
+          {
+            text: '斯拉克',
+            value: 14
+          },
+          {
+            text: '斯拉达',
+            value: 15
+          }
+        ]
+      ],
+      twoData: [
+        [
+          {
+            text: '剧毒',
+            value: 1
+          },
+          {
+            text: '蚂蚁',
+            value: 2
+          },
+          {
+            text: '幽鬼',
+            value: 3
+          },
+          {
+            text: '主宰',
+            value: 4
+          },
+          {
+            text: '卡尔',
+            value: 5
+          },
+          {
+            text: '宙斯',
+            value: 6
+          },
+          {
+            text: '巫医',
+            value: 7
+          },
+          {
+            text: '巫妖',
+            value: 8
+          },
+          {
+            text: '神谕者',
+            value: 9
+          },
+          {
+            text: '撼地神牛',
+            value: 10
+          },
+          {
+            text: '蓝胖子',
+            value: 11
+          },
+          {
+            text: '水晶室女',
+            value: 12
+          },
+          {
+            text: '莉娜',
+            value: 13
+          },
+          {
+            text: '斯拉克',
+            value: 14
+          },
+          {
+            text: '斯拉达',
+            value: 15
+          }
+        ],
+        [
+          {
+            text: '剧毒',
+            value: 1
+          },
+          {
+            text: '蚂蚁',
+            value: 2
+          },
+          {
+            text: '幽鬼',
+            value: 3
+          },
+          {
+            text: '主宰',
+            value: 4
+          },
+          {
+            text: '卡尔',
+            value: 5
+          },
+          {
+            text: '宙斯',
+            value: 6
+          },
+          {
+            text: '巫医',
+            value: 7
+          },
+          {
+            text: '巫妖',
+            value: 8
+          },
+          {
+            text: '神谕者',
+            value: 9
+          },
+          {
+            text: '撼地神牛',
+            value: 10
+          },
+          {
+            text: '蓝胖子',
+            value: 11
+          },
+          {
+            text: '水晶室女',
+            value: 12
+          },
+          {
+            text: '莉娜',
+            value: 13
+          },
+          {
+            text: '斯拉克',
+            value: 14
+          },
+          {
+            text: '斯拉达',
+            value: 15
+          }
+        ]
+      ],
+      threeData: [
+        [
+          {
+            text: '剧毒',
+            value: 1
+          },
+          {
+            text: '蚂蚁',
+            value: 2
+          },
+          {
+            text: '幽鬼',
+            value: 3
+          },
+          {
+            text: '主宰',
+            value: 4
+          },
+          {
+            text: '卡尔',
+            value: 5
+          },
+          {
+            text: '宙斯',
+            value: 6
+          },
+          {
+            text: '巫医',
+            value: 7
+          },
+          {
+            text: '巫妖',
+            value: 8
+          },
+          {
+            text: '神谕者',
+            value: 9
+          },
+          {
+            text: '撼地神牛',
+            value: 10
+          },
+          {
+            text: '蓝胖子',
+            value: 11
+          },
+          {
+            text: '水晶室女',
+            value: 12
+          },
+          {
+            text: '莉娜',
+            value: 13
+          },
+          {
+            text: '斯拉克',
+            value: 14
+          },
+          {
+            text: '斯拉达',
+            value: 15
+          }
+        ],
         [
           {
             text: '剧毒',
@@ -149,6 +415,29 @@ export default {
       let picker = this.$refs[`picker${index}`]
       // 获取到对应的picker，调用内部show方法
       picker.show()
+    },
+    change (i, index) {
+      console.log('change: ', i, index)
+    },
+    select (val, index, text) {
+      console.log('select: ', val, index, text)
+    },
+    valueChange (index, args) {
+      console.log(index)
+      switch (index) {
+        case 0:
+          this.oneColumn = args[2].join('--')
+          break
+        case 1:
+          this.twoColumn = args[2].join('--')
+          break
+        case 2:
+          this.threeColumn = args[2].join('--')
+          break
+        default:
+          break
+      }
+      console.log('valueChange: ', args)
     }
   },
   components: {
@@ -158,13 +447,13 @@ export default {
 </script>
 
 <style scoped>
-.bscroll-picker{
+.bscroll-picker {
   margin-top: 10px;
-  padding-top: 20px; 
+  padding-top: 20px;
   height: 500px;
   background-color: #d9d9d9;
 }
-.column{
+.column {
   width: 80%;
   line-height: 30px;
   margin: 0 auto 40px;
