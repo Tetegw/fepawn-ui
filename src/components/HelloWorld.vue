b73e665d5ea406752927837308e779277bedd956
孙晨 <1151832942@qq.com>
2017-12-27 17:14:16 +0800

    [fix] update dist

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,10 +8,10 @@
       <li><router-link to="/switch">Switch</router-link></li>
       <li><router-link to="/toast">Toast</router-link></li>
       <li><router-link to="/actionSheet">ActionSheet</router-link></li>
-      <li><router-link to="/buttonTab">buttonTab</router-link></li>
-      <li><router-link to="/betterScroll">betterScroll</router-link></li>
-      <li><router-link to="/waterRipple">waterRipple</router-link></li>
-      <li><router-link to="/dial">dial</router-link></li>
+      <li><router-link to="/buttonTab">ButtonTab</router-link></li>
+      <li><router-link to="/betterScroll">BetterScroll</router-link></li>
+      <li><router-link to="/waterRipple">WaterRipple</router-link></li>
+      <li><router-link to="/dial">Dial</router-link></li>
       <li><a href="https://tetegw.github.io/fepawn-ui/dist/static/readMe.html">文档说明</a></li>
     </ul>
   </div>
