563aae2443727463edcf46b0246044551da5d901
孙晨 <1151832942@qq.com>
2017-12-27 17:09:41 +0800

    [added] dial component done

--- src/components/Dial/index.vue
+++ src/components/Dial/index.vue
@@ -1,116 +1,211 @@
 <template>
   <div class="dial-wrap">
-    <div class="mask">
-      <canvas id="cv"></canvas>
-      <img src="./mask.png" alt="">
-    </div>
+    <canvas id="cv" class="dial-cv"></canvas>
+    <canvas id="cv2" class="dial-cv"></canvas>
   </div>
 </template>
 
 <script>
+const CVHEIGHT = 456           // 画布高度
+const CVWIDTH = 750            // 画布宽度
+const RADIUS = 241             // 半径
+const CIRCLEX = CVWIDTH / 2      // 圆心X
+const CIRCLEY = 370            // 圆心Y
+const LINEWIDTH = 27           // 圆环宽度
+// const STEP = 3                 // 每次渲染步数
+// const TIMEOUT = 10              // 速度
+const CIRCLEF = '#fdeee5'       // 圆环前景色
+// const PROP = 1                  // 比例【重要】
+// let START = 169.5               // 开始角度，完全是为切合表盘的图片，如果不用图片就用整数
+// const END = 169.5 + 201 * PROP   // 结束角度
 export default {
-  mounted () {
-    var cv = document.querySelector('#cv')
-    var ctx = cv.getContext('2d')
-    cv.height = 600
-    cv.width = 1000
-
-    function toRadian (angle) {
-      return angle / 180 * Math.PI
+  props: {
+    step: {
+      type: Number,
+      default: 3
+    },
+    timeOut: {
+      type: Number,
+      default: 10
+    },
+    rate: {
+      type: Number,
+      default: 0.4
     }
-
-    // function toAngle (radian) {
-    //   return radian / Math.PI * 180
-    // }
-
-    var data = [{
-      'value': 0.1,
-      'color': 'orange',
-      'title': '社会招生'
-    }, {
-      'value': 0.1,
-      'color': 'pink',
-      'title': '公务员'
-    }, {
-      'value': 0.1,
-      'color': 'gray',
-      'title': '公开课'
-    }, {
-      'value': 0.1,
-      'color': '#909090',
-      'title': '前端'
-    }, {
-      'value': 0.2,
-      'color': 'red',
-      'title': '应届生'
-    }, {
-      'value': 0.3,
-      'color': 'blue',
-      'title': '程序员'
-    }, {
-      'value': 0.1,
-      'color': '#abc',
-      'title': '老司机'
-    }]
-
-    var startAngle = -90 // 开始的角度（设置为-90度,sincos都好计算）
-    var r = 100 // 半径值
-    var rline = 30 // 半径延长线的长度
-    var x0 = 200 // 圆心坐标
-    var y0 = 200
-    var txtPadding = 10 // 文字到线的距离
-    var txtBottomLen = 0
-
-    // console.log(sum);
-    data.forEach(function (value) {
-      // 计算延长线角度
-      var textAngle = startAngle + value.value * 360 / 2
-      // 延长线x,y的坐标
-      var textX = x0 + (r + rline) * Math.cos(toRadian(textAngle))
-      var textY = y0 + (r + rline) * Math.sin(toRadian(textAngle))
-
-      // 绘制圆弧
-      ctx.beginPath()
-      ctx.moveTo(x0, y0)
-      ctx.fillStyle = value.color
-      ctx.arc(x0, y0, r, toRadian(startAngle), toRadian(startAngle += value.value * 360), false)
-      ctx.fill()
-
-      // 绘制直线
-      ctx.beginPath()
-      ctx.strokeStyle = value.color
-      ctx.moveTo(x0, y0)
-      ctx.lineTo(textX, textY)
-      ctx.moveTo(textX, textY)
-
-      // 在Y轴左边的 都是负值
-      ctx.font = '18px 微软雅黑'
-      var txtw = ctx.measureText(value.title).width  // 文本宽度
-      if (textX < x0) {
-        txtBottomLen = -txtw
-        txtPadding = -Math.abs(txtPadding)
-        ctx.textAlign = 'right'
-      } else {
-        txtBottomLen = txtw
+  },
+  created () {
+    this.START = 169.5
+    this.END = this.START + 201 * this.rate   // 结束角度
+    this.animationFrame()
+    this.animationRender()
+  },
+  mounted () {
+    this.drawCanvas()
+  },
+  methods: {
+    // 封装requestAnimationFrame兼容性
+    animationFrame () {
+      var lastTime = 0
+      var prefixes = 'webkit moz ms o'.split(' ') // 各浏览器前缀
+      var requestAnimationFrame = window.requestAnimationFrame
+      var cancelAnimationFrame = window.cancelAnimationFrame
+      var prefix
+      // 通过遍历各浏览器前缀，来得到requestAnimationFrame和cancelAnimationFrame在当前浏览器的实现形式
+      for (var i = 0; i < prefixes.length; i++) {
+        if (requestAnimationFrame && cancelAnimationFrame) {
+          break
+        }
+        prefix = prefixes[i]
+        requestAnimationFrame = requestAnimationFrame || window[prefix + 'RequestAnimationFrame']
+        cancelAnimationFrame = cancelAnimationFrame || window[prefix + 'CancelAnimationFrame'] || window[prefix + 'CancelRequestAnimationFrame']
+      }
+      // 如果当前浏览器不支持requestAnimationFrame和cancelAnimationFrame，则会退到setTimeout
+      if (!requestAnimationFrame || !cancelAnimationFrame) {
+        requestAnimationFrame = function (cb, element) {
+          var currTime = new Date().getTime()
+          // 为了使setTimteout的尽可能的接近每秒60帧的效果
+          var timeToCall = Math.max(0, 16 - (currTime - lastTime))
+          var id = window.setTimeout(function () {
+            // cb && cb(currTime + timeToCall)
+          }, timeToCall)
+          lastTime = currTime + timeToCall
+          return id
+        }
+        cancelAnimationFrame = function (id) {
+          window.clearTimeout(id)
+        }
+      }
+      // 得到兼容各浏览器的API
+      window.requestAnimationFrame = requestAnimationFrame
+      window.cancelAnimationFrame = cancelAnimationFrame
+    },
+    // 封装动画渲染函数，以及出口
+    animationRender (callback, time) {
+      var lastFrameTime = new Date() - 0
+      var callbackRes = true
+      function render () {
+        var curFrameTime = new Date() - 0
+        var interval = curFrameTime - lastFrameTime
+        if (interval >= time) {
+          lastFrameTime = curFrameTime
+          callbackRes = callback()
+        }
+        if (callbackRes) {
+          window.requestAnimationFrame(render)
+        }
+      }
+      render()
+    },
+    drawCanvas () {
+      var _this = this
+      // 角度转弧度
+      function toRadian (angle) {
+        return angle / 180 * Math.PI
       }
 
-      ctx.lineTo(textX + txtBottomLen + txtPadding * 2, textY)
-      ctx.stroke()
+      // 绘制渐变背景
+      function backGradient () {
+        var grd = ctx.createLinearGradient(CVWIDTH / 2, 0, CVWIDTH / 2, CVHEIGHT)
+        grd.addColorStop(0, '#ff9232')
+        grd.addColorStop(0.5, '#fe7d2a')
+        grd.addColorStop(1, '#ff7a0e')
+        ctx.fillStyle = grd
+        ctx.fillRect(0, 0, CVWIDTH, CVHEIGHT)
+      }
 
-      // 绘制文字
-      ctx.fillText(value.title, textX + txtPadding, textY - 8)
-    })
+      // 绘制圆环背景
+      /* function backgroundCircle(){
+          ctx.save()
+          ctx.strokeStyle = CIRCLEB
+          ctx.lineWidth = LINEWIDTH
+          ctx.lineCap = "round"
+          ctx.beginPath()
+          ctx.arc(CIRCLEX, CIRCLEY, RADIUS, toRadian(START), toRadian(START+200), false)
+          ctx.stroke()
+          ctx.closePath()
+          ctx.restore()
+      } */
+
+      // 绘制运动圆环
+      function foregroundCircle () {
+        ctx.save()
+        ctx.strokeStyle = CIRCLEF
+        ctx.lineWidth = LINEWIDTH
+        ctx.lineCap = 'round'
+        ctx.beginPath()
+        ctx.arc(CIRCLEX, CIRCLEY, RADIUS, toRadian(_this.START), toRadian(_this.START += _this.step), false)
+        ctx.stroke()
+        ctx.closePath()
+        ctx.restore()
+      }
+
+      // 绘制圆环头部，用cv2画，然后合成
+      function circleHeader (img) {
+        ctx2.clearRect(0, 0, cv.width, cv.height)
+        var imgW = img.width
+        var imgH = img.height
+        var x = Math.cos(toRadian(_this.START + _this.step)) * RADIUS + CIRCLEX - imgW / 2
+        var y = Math.sin(toRadian(_this.START + _this.step)) * RADIUS + CIRCLEY - imgH / 2
+        ctx2.drawImage(img, 0, 0, imgW, imgH, x, y, imgW, imgH)
+      }
+
+      // 所有图片加载完成后执行
+      function imgload (imgarr, callback) {
+        var imgObj = {}
+        var loaded = 0
+        imgarr.forEach(function (value) {                         // 遍历加载完成
+          var img = new Image()
+          img.src = 'static/images/' + value + '.png'
+          img.addEventListener('load', function () {            // 监听加载事件
+            loaded++                                          // loaded++
+            imgObj[value] = img                             // 储存img对象
+            if (loaded >= imgarr.length) {                  // 判断loaded是否达到总长度
+              callback(imgObj)                              // 回调函数调用
+            }
+          })
+        })
+      }
+
+      // canvas 绘制开始
+      var cv = document.querySelector('#cv')
+      var ctx = cv.getContext('2d')
+      cv.height = CVHEIGHT
+      cv.width = CVWIDTH
+      var cv2 = document.querySelector('#cv2')
+      var ctx2 = cv2.getContext('2d')
+      cv2.height = CVHEIGHT
+      cv2.width = CVWIDTH
+
+      var imgArr = ['background', 'circle']
+      imgload(imgArr, function (imgObj) {
+        backGradient()
+        var imgW = imgObj['background'].width
+        var imgH = imgObj['background'].height
+        ctx.drawImage(imgObj['background'], 0, 0, imgW, imgH, 0, 0, imgW, imgH)
+        // 绘制动画
+        _this.animationRender(function () {
+          if (_this.START > _this.END - _this.step) {
+            return false
+          }
+          circleHeader(imgObj['circle'])
+          foregroundCircle()
+          return true
+        }, _this.timeOut)
+      })
+    }
   }
 }
 </script>
 
-<style>
-.mask {
+<style scoped>
+.dial-wrap {
   width: 100%;
-  background-color: #f59d69;
-  font-size: 0;
+  position: relative;
 }
-.mask img {
+.dial-cv {
   width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
--- src/components/_ModulePage/dial.vue
+++ src/components/_ModulePage/dial.vue
@@ -1,16 +1,51 @@
 <template>
-	<v-dial></v-dial>
+	<div>
+    <v-dial
+      v-if="showDial"
+      :step="5"
+      :timeOut="10"
+      :rate="Number(rate)"
+    ></v-dial>
+    <div class="input">
+      <input class="input-item" type="number" v-model="rate" @keydown.enter="drawCanvas">
+      <button class="input-item" @click="drawCanvas">重新渲染</button>
+    </div>
+  </div>
 </template>
 
 <script>
 import Dial from '@/components/Dial'
 export default {
+  data () {
+    return {
+      rate: 0.5,
+      showDial: true
+    }
+  },
+  methods: {
+    drawCanvas () {
+      console.log('object')
+      this.showDial = false
+      this.$nextTick(() => {
+        this.showDial = true
+      })
+    }
+  },
   components: {
     'v-dial': Dial
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+.input{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate3d(-50%, -50%, 0);
+}
+.input-item{
+  margin-top: 10px;
+  border: 1px solid #d9d9d9;
+}
 </style>
